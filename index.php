cbef8e8089bd7fe7c7fb9277e89b2879c3e88804
Iago Rossi <iagogasparetto@hotmail.com>
2020-09-05 16:50:54 -0300

    comit inicial

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width-device.width, initial.scale = 1">
+</head>
+
+<body>
+
+    <P>FORMULÁRIO PARA A INSCRIÇÃO DE NADADORES</P>
+
+
+<!---aciton informa qua o endereço da url eu quero subter os dados--->
+<form action="script.php" method="post"> <!---method iinforma o tipo de envio do formulario, get or post--->
+    <P>Seu nome: <input type="text" name = "nome"></P>
+    <p>Sua idade: <input type="text" name = "idade"></p>
+    <p><input type="submit" value="Enviar dado do competidor"></p>
+
+</form>
+
+</body>
+
+</html>
